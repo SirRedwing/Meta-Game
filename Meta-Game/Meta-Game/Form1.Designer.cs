0fe6e8d6ab32056328f3aab222c11f07ffd54563
Collin Campbell <blackwold10101@gmail.com>
2018-04-12 14:23:20 -0600

    Solution upload

---
+++ Meta-Game/Meta-Game/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Meta_Game {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.primaryTextbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Menu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // primaryTextbox
+            // 
+            this.primaryTextbox.Font = new System.Drawing.Font("MS Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.primaryTextbox.Location = new System.Drawing.Point(12, 357);
+            this.primaryTextbox.Multiline = true;
+            this.primaryTextbox.Name = "primaryTextbox";
+            this.primaryTextbox.ReadOnly = true;
+            this.primaryTextbox.Size = new System.Drawing.Size(1336, 101);
+            this.primaryTextbox.TabIndex = 2;
+            this.primaryTextbox.Text = resources.GetString("primaryTextbox.Text");
+            this.primaryTextbox.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 461);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(400, 80);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(488, 461);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(400, 80);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(948, 461);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(400, 80);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.Menu.Location = new System.Drawing.Point(1273, 12);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(75, 25);
+            this.Menu.TabIndex = 6;
+            this.Menu.Text = "Menu";
+            this.Menu.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1360, 553);
+            this.Controls.Add(this.Menu);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.primaryTextbox);
+            this.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox primaryTextbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button Menu;
+    }
+}
+
---
+++ Meta-Game/Meta-Game/Program.cs
@@ -0,0 +1,29 @@
+﻿// File/Project Prolog
+// Name:     <PUT YOUR NAME HERE>
+// Period:   <PUT YOUR PERIOD HERE>
+// Username: campbcol007
+// Project:  Meta_Game
+// Date:     4/12/2018 1:42:57 PM
+//
+// I declare that the following code was written by me or provided 
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+// ---------------------------------------------------------------------------
+
+using System;
+using System.Windows.Forms;
+
+namespace Meta_Game {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
