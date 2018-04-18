984c2272e69693c324d07abcfbef450d520b9c44
SirRedwing <blackwold10101@gmail.com>
2018-04-18 04:00:20 -0600

    Stylistic changes

--- Rogue Program Abort.bat
+++ Rogue Program Abort.bat
@@ -1,6 +1,6 @@
 @echo off
 set /p appName="Enter rogue program name: "
-echo Shutting down rogue program...
+echo Shutting down rogue program. . .
 timeout /t 3 /nobreak > nul
 TASKKILL /F /IM %appName% > nul
 echo Command execution complete.
